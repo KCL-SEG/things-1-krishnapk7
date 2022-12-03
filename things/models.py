db73470857bb28a516d3deba121e4631c484dc4b
Krishna <krishna.prasanna_kumar@kcl.ac.uk>
2022-12-03 17:55:52 +0000

    Add Model

--- things/models.py
+++ things/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Thing(models.model):
+class Thing(models.Model):
     name = models.CharField(max_length=30, blank=False)
     description = models.CharField(max_length=150, blank=False)
     quantity = models.PositiveIntegerField(default=1)
